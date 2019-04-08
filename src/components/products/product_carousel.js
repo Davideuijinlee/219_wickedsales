168429f4da6ca0748970decc7bfee0f7c38a496d
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-08 16:08:13 -0700

    added misc details add price added image carousel

--- src/components/products/product_carousel.js
+++ src/components/products/product_carousel.js
@@ -3,8 +3,12 @@ import React, {Component} from 'react';
 class ProductCarousel extends Component {
     componentDidMount(){
         console.log('Carousel Div:', this.carousel);
+        const config = {
+            numVisible: 1,
+            indicators: true
+        };
 
-        M.Carousel.init(this.carousel)
+        M.Carousel.init(this.carousel, config);
     }
     render(){
         console.log('Props:', this.props);
@@ -16,7 +20,7 @@ class ProductCarousel extends Component {
             )
         });
         return (
-            <div ref={(element) => this.carousel = element} className="carousel">
+            <div ref={(element) => this.carousel = element} className="col s12 m8 carousel">
                 {items}
             </div>
         )
--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import axios from'axios';
 import ProductCarousel from './product_carousel'
+import MiscDetails from './misc_details';
+import {formatMoney} from '../../helpers';
+
 
 class ProductDetails extends Component{
     state ={
@@ -41,13 +44,34 @@ class ProductDetails extends Component{
             return <h1 className="center">No Product Found</h1>
         }
 
-        const {description='No description available', name, images, price} = details;
+        const {description='No description available', name, images, price, miscDetails} = details;
 
         return(
             <div className="product-details">
                 <h1 className="center">{name}</h1>
-                <ProductCarousel images={images}/>
-                <p>{description}</p>
+                <div className="row">
+                    <ProductCarousel images={images}/>
+                    <div className="col s12 m4">
+                    <div className="right-align product-price">{formatMoney(price)}</div>
+                    <div className="right-align add-to-cart">
+                        <span className="qty-container">
+                            <button className="btn-small btn-floating red lighten-1">
+                                <i className="material-icons">remove</i>
+                            </button>
+                                <span className="product-qty">1</span>    
+                            <button className="btn-small btn-floating blue lighten-1">
+                                <i className="material-icons">add</i>
+                            </button>
+                        </span>
+                        <button className="btn purple darken-1">
+                            <i className="material-icons">add_shopping_cart</i>
+                        </button>
+                    </div>
+                        <p>{description}</p>
+                        <MiscDetails details={miscDetails}/>
+                    </div>
+                </div>
+                
             </div>
         );
     }
