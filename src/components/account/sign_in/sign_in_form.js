cc1809291709f8452c314ba7f8e14185c13708c9
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-15 18:05:29 -0700

    client auth

--- src/components/account/sign_in/sign_in_form.js
+++ src/components/account/sign_in/sign_in_form.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
+import Input from '../../general/input';
 
 const SignInForm = props => {
+    console.log('Sign in Form Props', props);
+    const{handleSubmit, signIn} = props;
     return(
-        <form>
-            <h1>Form Goes Here</h1>
+        <form onSubmit={handleSubmit(signIn)}>
+                <Field id="email" name="email" component={Input} label="Email"/>
+
+                <Field id="password" name="password" component={Input} type="password" label="password"/>
+            <div className="row">
+                <div className="col s12 right-align">
+                    <button className="btn blue lighten-1">Sign In</button>
+                </div>
+            </div>
         </form>
     )
 }
